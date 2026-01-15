5380b6e435df14c93ad5998aed8301a712718bb9
pgrxv <yeepaulchristian@gmail.com>
2026-01-15 00:50:46 -0500

    add lab  solution

---
+++ Week01/Lab1.py
@@ -0,0 +1,16 @@
+# Using Hash
+
+#2 
+number = [1, 4, 7, 9]
+
+#3
+a, b, c, d = 1, 2, 3, 4
+
+e = a - ((b ** c) // d) + (a % c)
+
+
+#4
+userAge = int(input("Enter Age: "))
+userAge = userAge + 22
+print("The new age is ", userAge)
+
