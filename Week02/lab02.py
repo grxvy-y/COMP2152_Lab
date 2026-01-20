9655beacf15878a5bbf25e158e8bfe71ab40856a
pgrxv <yeepaulchristian@gmail.com>
2026-01-20 10:41:22 -0500

    add the choices array, and get the user input

--- Week02/lab02.py
+++ Week02/lab02.py
@@ -1,3 +1,7 @@
 # This is the Python file for the Week 2
 # Paul Christian Yee
 
+choices = ["Rock", "Paper", "Scissors"]
+
+playerChoice = input("Enter your choice (1-Rock, 2-Paper, 3-Scissors): ")
+
